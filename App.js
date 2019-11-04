c4643e34390ecb5a53d438f221d9e5df6577b6c5
Cesar Victorio <cbvictorio@gmail.com>
2019-11-03 18:28:01 -0800

    uploaded initial config files to run a react project

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const App = () => (
+    <div>
+        <h1> Hello from main app </h1>
+        <p> Hello </p>
+    </div>
+)
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}

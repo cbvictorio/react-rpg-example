ccb94c1303b3c1ba581b70d18b12955d84fdcf60
Cesar Victorio <cbvictorio@gmail.com>
2019-11-05 00:16:45 -0800

    included Provider component on App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
 import { App } from '../App'
+import { Store } from './store/'
+
 
 const container = document.getElementById('app')
 
-ReactDOM.render(<App />, container)
+ReactDOM.render(
+    <Provider store={Store}>
+        <App /> 
+    </Provider>
+    , 
+    container
+)
